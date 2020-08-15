7ba2584e9ff4557c8367db9937bd9f146c458c4c
Shivansh Rakesh <shivanshrakesh@gmail.com>
2020-08-15 15:00:39 +0530

    added mosaic

--- res/scripts/init.js
+++ res/scripts/init.js
@@ -4,6 +4,7 @@
 // })(jQuery);
 
 function init() {
+    document.getElementById("load").style.opacity = '0';
     $("#camera-gif-figure").animate({
         // width and max-width have to be changed accordingly in index.css > #camera-gif
         top: String(0.5 * $(window).height() - (Math.min(0.15 * $(window).width(), 150))) + "px",
